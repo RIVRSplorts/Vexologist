020fb9ad3034906f3aeda347826291a125c3a332
Saimons <contact@jwsy.co.uk>
2021-08-25 18:21:30 +0100

    changes to player parsing to use the latest player name/team. this won't work for historical data but is a temporary hack

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class vexologist(object):
             self.cur = self.conn.cursor()
             self.cur.execute('''CREATE TABLE "Racers" (
 	    "Racer_ID"        INTEGER,
-	    "Emoji"           TEXT,
+	    "Emoji"           TEXT UNIQUE,
 	    "Name"	      TEXT,
             "Team"            TEXT,
             "Races"           INTEGER DEFAULT 0,
@@ -52,14 +52,16 @@ class vexologist(object):
             PRIMARY KEY("Race_ID")
             )''')
 
-
             
-            self.conn.commit()
         else:
             #has to be duplicated as otherwise sqlite3.connect creates the database before the if check
             self.conn = sqlite3.connect(database)
             self.cur = self.conn.cursor()
 
+        #populate with racers
+ 
+        self.update_racers()
+          
         self.pit_stop= re.compile("(.*) waved from the Pit Stop!")
         self.bonk_S  = re.compile("bonked .* off the track")
         self.bonk_F  = re.compile("bonked into")
@@ -91,15 +93,37 @@ class vexologist(object):
         #if there's an empty return, setup their record. note the defaults for stats are 0 so this also populates those columns implicitly
         #if return isn't empty, convert the return tuple to a list and stow it in the dict
         if not ret:
-            self.cur.execute("INSERT INTO racers (Emoji, Name, Races, Team) VALUES (?,?,?,?)",(emoji,name,1,team))
+            self.update_racers()
             racer_temp_totals = [0,0,0,0,0, 0,0,0,0,0]
         else:
             racer_temp_totals = list(ret[5:])
-            self.cur.execute("UPDATE Racers SET Races =?, Team =? WHERE Emoji = ?",(ret[4]+1,team ,emoji))
+            #ignore which team they're on during parsing as that gets updated at the end
+            self.cur.execute("UPDATE Racers SET Races =? WHERE Emoji = ?",(ret[4]+1,emoji))
 
         self.conn.commit()
         return racer_temp_totals
+
     
+    def update_racers(self):
+        #update or insert all record of active racers
+        last_update = sorted(listdir('../json/racers/'))[-1]
+        with open(path.join('../json/racers/',last_update), 'r') as f:
+            racers_file = json.load(f)
+            
+        allplayer_list = [racers_file["inactive"],racers_file["active"]]
+
+        for active_status, player_sublist in enumerate(allplayer_list):
+            for player in player_sublist.items():
+                name = player[0]
+                emoji = player[1]["emoji"]
+                if "team" in player[1]:
+                    team  = player[1]["team"]
+                else:
+                    team = None
+                self.cur.execute("INSERT OR IGNORE into Racers (Emoji,Team, Name) VALUES (?,?,?)",(emoji,team,name))
+        self.conn.commit()
+
+
     #parse race and update racers records
     def parse_race(self,race_json):
         colnames = ["Firsts","Seconds","Thirds","Fourths","Fifths","Sixths","Sevenths","Eighths"]
@@ -148,7 +172,7 @@ class vexologist(object):
                 steal_re = self.steal.search(line)
                 stolen_from = steal_re.group(2)[0]
                 racers_temp_totals[line[2]] = [0,0,0,0,0, 0,0,0,0,0]
-                self.insert_racer(line[2],'new_racer?','uhh')#need to link this to peeps.json to pull out who it is
+                self.insert_racer(line[2],'new_racer?',None)#need to link this to peeps.json to pull out who it is
 
                 
             elif self.voided.search(line):
@@ -157,7 +181,7 @@ class vexologist(object):
                 voided = void_re.group(1)[2]
                 emoji = void_re.group(2)[0]
                 racers_temp_totals[emoji] = [0,0,0,0,0, 0,0,0,0,0]
-                self.insert_racer(emoji,'new_racer?','uhh')#need to link this to peeps.json to pull out who it is
+                self.insert_racer(emoji,'new_racer?',None)#need to link this to peeps.json to pull out who it is
 
                 
             elif self.bonk_S.search(line):
@@ -228,3 +252,5 @@ if __name__ == "__main__":
             json_dump = json.load(f)
         datahandler.parse_race(json_dump)
     
+
+    datahandler.update_racers()
